194460638ae7d503ce245aaaf4209e73e8892132
lchen104 <lchen@zdmedia.com>
2023-08-03 09:58:17 -0400

    Added Counter.module.css for styling - Final Commit

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { decrement, increment } from './counterSlice';
-// import styles from './Counter.module.css'
+import styles from './Counter.module.css'
 
 const Counter = () => {
 
@@ -10,15 +10,17 @@ const Counter = () => {
 
     return (
     <div>
-        <div>
+        <div className={styles.row}>
           <button
+            className={styles.button}
             aria-label="Increment value"
             onClick={() => dispatch(increment())}
           >
             Increment
           </button>
-          <span>{count}</span>
+          <span className={styles.value}>{count}</span>
           <button
+            className={styles.button}
             aria-label="Decrement value"
             onClick={() => dispatch(decrement())}
           >
